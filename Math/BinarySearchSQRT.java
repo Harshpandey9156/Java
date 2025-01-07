68d4b0e8c69b0fce28be5ec5679d43c7486a091a
Harshpandey9156 <harshpandey21012@gmail.com>
2025-01-07 23:21:54 +0530

    yes

--- Math/BinarySearchSQRT.java
+++ Math/BinarySearchSQRT.java
@@ -5,13 +5,46 @@ public class BinarySearchSQRT {
         int n=40;
         int p=3;
 
-        System.out.println(sqrt(n,p));
+        System.out.printf("%.3f", sqrt(n,p));
 
     }
 
     static double sqrt(int num, int p){
 
-        return p;
+        int s=0;
+        int e=num;
+        
+        double root=0.0;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+
+            if(mid*mid==num ){
+                return mid;
+            }
+            if(mid*mid>num){
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
+
+        }
+
+
+        double incr=0.1;
+        for(int i=0;i<p;i++){
+            while(root*root<=num){
+                root+=incr;
+            }
+            root-=incr;
+            incr/=10;
+        }
+
+
+
+
+
+        return root;
 
     }
 }
