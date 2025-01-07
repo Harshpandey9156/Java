7b3394ad293b626868310a9895b6174642b8393d
Harshpandey9156 <harshpandey21012@gmail.com>
2025-01-07 20:31:16 +0530

    yes

---
+++ Math/BinarySearchSQRT.java
@@ -0,0 +1,17 @@
+package Math;
+
+public class BinarySearchSQRT {
+    public static void main(String[] args) {
+        int n=40;
+        int p=3;
+
+        System.out.println(sqrt(n,p));
+
+    }
+
+    static double sqrt(int num, int p){
+
+        return p;
+
+    }
+}
